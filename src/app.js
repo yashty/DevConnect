eb61a176bccbb7b7e1f3886f00e80a7418d90d07
yashty <yashtyagi2612@gmail.com>
2025-03-03 15:36:43 +0530

    add data validation and data sanitazation

--- src/app.js
+++ src/app.js
@@ -75,11 +75,29 @@ app.delete("/user", async (req, res) => {
   }
 });
 
-app.patch("/user", async (req, res) => {
-  const userId = req.body.userId;
+app.patch("/user/:userId", async (req, res) => {
+  const userId = req.params?.userId;
   const data = req.body;
+
   try {
-    const user = await User.findByIdAndUpdate({ _id: userId }, data);
+    const ALLOWED_UPDATES = ["skills", "photoUrl", "about", "gender", "age"];
+
+    const isUpdateAllowed = Object.keys(data).every((k) =>
+      ALLOWED_UPDATES.includes(k)
+    );
+
+    if (!isUpdateAllowed) {
+      throw new Error("Update not allowed");
+    }
+
+    if (data?.skills.length > 10) {
+      throw new Error("Skills cannot be more than 10");
+    }
+
+    const user = await User.findByIdAndUpdate({ _id: userId }, data, {
+      runValidators: true,
+    });
+
     res.status(200).json({ msg: "User updated successfully." });
   } catch (error) {
     console.error(error);
--- src/models/user.js
+++ src/models/user.js
@@ -1,24 +1,65 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const userSchema = new mongoose.Schema(
   {
     firstName: {
       type: String,
+      required: true,
+      minLength: 4,
+      maxLength: 50,
     },
     lastName: {
       type: String,
     },
     emailId: {
       type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Invalid Email address" + value);
+        }
+      },
     },
     password: {
       type: String,
+      required: true,
+      validate(value) {
+        if (!validator.isStrongPassword(value)) {
+          throw new Error("Enter a strong password");
+        }
+      },
     },
     age: {
       type: Number,
+      min: 18,
     },
     gender: {
       type: String,
+      validate(value) {
+        if (!["Male", "Female", "Others"].includes(value)) {
+          throw new Error("Gender data is not valid.");
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+      default:
+        "https://www.cgg.gov.in/wp-content/uploads/2017/10/dummy-profile-pic-male1.jpg",
+      validate(value) {
+        if (!validator.isURL(value)) {
+          throw new Error("Invalid Photo URL" + value);
+        }
+      },
+    },
+    about: {
+      type: String,
+      default: "This is a default about of the user!",
+    },
+    skills: {
+      type: [string],
     },
   },
   { timestamps: true }
